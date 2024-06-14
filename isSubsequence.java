a002cdcfe75e10627490af79c1a487614eeeeac5
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-06-14 18:22:03 -0400

    Create isSubsequence.java

---
+++ isSubsequence.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+//2 pointers approach
+// T.C = O(n) where n is len(t) or len(s) whichever is longer
+// S.C = O(1)
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        if(s.length()==0)
+            return true;
+
+        boolean result = false;
+        int k = 0;
+
+        for (int i = 0; i < t.length(); i++)
+        {
+            if(s.charAt(k)==t.charAt(i)){
+                k++;
+            }
+
+            if(k>=s.length()){
+                result = true;
+                break;
+            }
+
+        }
+
+        return  result;
+    }
+}
