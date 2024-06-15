fec3bc1c1980e2a139ef1d21693e18c2ca209853
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-06-15 10:45:55 -0400

    Update LengthOfLastWord.java

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,9 +1,17 @@
 //https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
-//not the optimal solution
-//using library methods
 class Solution {
     public int lengthOfLastWord(String s) {
-        String sub[]= s.stripLeading().stripTrailing().split(" ");
-        return sub[sub.length-1].length();
+         int len = 0;
+        
+        for (int i =s.length()-1; i >=0 ; i--)
+        {
+            if(s.charAt(i)!=' '){
+                len++;
+            }else if(len>0){
+                break;
+            }
+
+        }
+        return len;
     }
 }
