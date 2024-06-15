21e9dd3230cc3348739343f3af34c0cde5df31c3
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-06-14 20:32:24 -0400

    Create ValidParenthesis.java

---
+++ ValidParenthesis.java
@@ -0,0 +1,52 @@
+//https://leetcode.com/problems/valid-parentheses/?envType=study-plan-v2&envId=top-interview-150
+class Solution {
+    public boolean isValid(String s) {
+        char topBracket;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i <s.length() ; i++)
+        {
+            char currentBracket = s.charAt(i);
+
+            if(currentBracket=='(' || currentBracket=='{' || currentBracket=='[')
+            {
+                stack.push(currentBracket);
+            }else{
+                if(stack.isEmpty())
+                {
+                   return false;
+                }
+
+                topBracket = stack.pop();
+                switch (topBracket){
+                    case '{':{
+                        if(currentBracket!='}'){
+                           return  false;
+                        }
+                    }
+                    break;
+                    case '[':{
+                        if(currentBracket!=']'){
+                            return  false;
+                        }
+                    }
+                    break;
+                    case '(':{
+                        if(currentBracket!=')'){
+                            return  false;
+                        }
+                    }
+                    break;
+                    default:
+                        return false;
+                }
+
+            }
+
+        }
+
+        return stack.isEmpty();
+
+
+    }
+}
