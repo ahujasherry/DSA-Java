56ef3f4850af4da01dd95e29fb5aeec5e2eab11f
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-07-03 12:41:25 -0400

    Create LongestCommonSubsequence.java

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/longest-common-subsequence/
+class Solution {
+    public int longestCommonSubsequence(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int dp[][] = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int ind1 = 1; ind1 <= m; ind1++) {
+            for (int ind2 = 1; ind2 <= n; ind2++) {
+                if (str1.charAt(ind1 - 1) == str2.charAt(ind2 - 1)) {
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                } else {
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                }
+
+            }
+        }
+
+        return dp[m][n];
+    }
+
+}
