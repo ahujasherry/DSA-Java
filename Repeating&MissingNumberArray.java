8c57a3427acf4bc410341c3a9fba9bdcb64b9010
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-07-24 10:01:20 -0400

    Update Repeating&MissingNumberArray.java
    XOR solution

--- Repeating&MissingNumberArray.java
+++ Repeating&MissingNumberArray.java
@@ -1,42 +1,56 @@
-import java.util.*;
-
-public class tUf {
-
-    public static int[] findMissingRepeatingNumbers(int[] a) {
-        long n = a.length; // size of the array
-        // Find Sn and S2n:
-        long SN = (n * (n + 1)) / 2;
-        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
-
-        // Calculate S and S2:
-        long S = 0, S2 = 0;
+public class Solution {
+   
+    public int[] repeatedNumber(final int[] A)
+    {
+       int n  = A.length;
+       int xr = 0;
+       
+        for(int i=0;i<n;i++)
+        {
+            xr = xr ^ A[i];
+            xr = xr ^ (i+1);
+        }
+        
+        //xr = X ^ Y ( repeating and missing)
+        
+        
+        //find difference bit
+        int number  = (xr & ~(xr - 1));
+       
+        
+        //group in 1/0 based on set bit
+        int zero =0;   //0
+        int one = 0;   //1
+        
+        for(int i=0;i<n;i++)
+        {
+            if( (number & A[i]) !=0){
+                one = one^ A[i];
+            }else{
+                zero = zero^ A[i];
+            }
+        }
+        
+        //1 to n
+         for(int i=1;i<=n;i++)
+        {
+            if((i&number)!=0){
+                one = one^ i;
+            }else{
+                zero = zero^ i;
+            }
+        }
+        
+        
+      // Last step: Identify the numbers:
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
-            S += a[i];
-            S2 += (long)a[i] * (long)a[i];
+            if (A[i] == zero) cnt++;
         }
 
-        //S-Sn = X-Y:
-        long val1 = S - SN;
-
-        // S2-S2n = X^2-Y^2:
-        long val2 = S2 - S2N;
-
-        //Find X+Y = (X^2-Y^2)/(X-Y):
-        val2 = val2 / val1;
-
-        //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
-        // Here, X-Y = val1 and X+Y = val2:
-        long x = (val1 + val2) / 2;
-        long y = x - val1;
-
-        int[] ans = {(int)x, (int)y};
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
-        int[] ans = findMissingRepeatingNumbers(a);
-        System.out.println("The repeating and missing numbers are: {"
-                           + ans[0] + ", " + ans[1] + "}");
+        if (cnt == 2) return new int[] {zero, one};
+        return new int[] {one, zero};
+        
+       
     }
 }
