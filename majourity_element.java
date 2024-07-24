09f0dc7bfe0a4822e08ecd0419fdc74015c36dde
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-07-23 21:12:57 -0400

    Update majourity_element.java

--- majourity_element.java
+++ majourity_element.java
@@ -3,22 +3,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int majourityEle = nums[0];
-        int count = 1;
+        int majourityEle = -1;
+        int count = 0;
 
-        for (int i = 1; i <n ; i++)
+        for (int i = 0; i <n ; i++)
         {
-            if(majourityEle == nums[i])
+            if(count<=0)
+            {
+                majourityEle = nums[i];
+                count=1;
+            }
+
+            else if(majourityEle == nums[i])
             {
                 count++;
             }else{
                 count--;
             }
 
-            if(count<=0){
-                majourityEle = nums[i];
-                count=1;
-            }
+            
 
         }
         return majourityEle;
