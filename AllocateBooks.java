87da38973f7d1ce20733b3bffbb46515f4c2875d
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-07-26 13:07:31 -0400

    Update AllocateBooks.java
    add binary search solution

--- AllocateBooks.java
+++ AllocateBooks.java
@@ -1,6 +1,43 @@
 //https://www.interviewbit.com/problems/allocate-books/
 //T.C =  O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.
 public class Solution {
+
+    //T.C =  O(N * log(sum(arr[])-max(arr[])+1)),
+      public int booksBS(int[] pages, int totalStudents)
+    {
+        
+        int n = pages.length;
+        int finalAns = 0;
+        
+        if(totalStudents> n ) return -1;
+        
+        int low = 0;    //max pages
+        long high = 0;  // sum of pages
+        
+        for(int ele : pages)
+        {
+            low = Math.max(low, ele);
+            high = high + ele;
+        }
+        
+        while (low <= high)
+        {
+            int mid = (int) ((low + high) / 2);
+            if (allocationPossible(pages, mid,totalStudents, n)) 
+            {
+                finalAns = mid;
+                high = mid-1;  // Try for a smaller max page count
+            } else {
+                low = mid + 1;  // Increase the max page count
+            }
+        }
+        
+        return finalAns;
+       
+        
+    }
+
+    //T.C =  O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements
     public int books(int[] pages, int totalStudents)
     {
         
