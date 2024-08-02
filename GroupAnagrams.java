9690583f86c0b15b92b597873fa41105d81decf3
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-08-01 21:34:36 -0400

    Create GroupAnagrams.java

---
+++ GroupAnagrams.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+        int n = strs.length;
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            char charArray[] = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if(!map.containsKey(sortedStr))
+            {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            
+            map.get(sortedStr).add(strs[i]);
+            
+        }
+        
+        for(Map.Entry<String, List<String>> entry: map.entrySet())
+        {
+            ans.add(new ArrayList<>(entry.getValue()));
+        }
+        return ans;
+
+    }
+}
