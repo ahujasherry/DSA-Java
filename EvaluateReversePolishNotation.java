eb01491b8f93a80613e4e2b96330df3cbb681f54
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-08-17 13:39:59 -0400

    Update EvaluateReversePolishNotation.java

--- EvaluateReversePolishNotation.java
+++ EvaluateReversePolishNotation.java
@@ -2,30 +2,36 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
 
-        for (String c : tokens)
-        {
-            if (c.equals("+")) 
-            {
-                stack.push(stack.pop() + stack.pop());
-            } else if (c.equals("-")) 
-            {
+        for (String c : tokens) {
+            if (c.equals("+")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                long result = (long) first + (long) second;
+                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                    throw new ArithmeticException("Integer overflow in addition");
+                }
+                stack.push((int) result);
+            } else if (c.equals("-")) {
                 int second = stack.pop();
                 int first = stack.pop();
                 stack.push(first - second);
-            } else if (c.equals("*"))
-            {
-                stack.push(stack.pop() * stack.pop());
-            } else if (c.equals("/")) 
-            {
+            } else if (c.equals("*")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                long result = (long) first * (long) second;
+                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                    throw new ArithmeticException("Integer overflow in multiplication");
+                }
+                stack.push((int) result);
+            } else if (c.equals("/")) {
                 int second = stack.pop();
                 int first = stack.pop();
                 stack.push(first / second);
-            } else 
-            {
+            } else {
                 stack.push(Integer.parseInt(c));
             }
         }
 
-        return stack.peek();        
+        return stack.peek();
     }
 }
