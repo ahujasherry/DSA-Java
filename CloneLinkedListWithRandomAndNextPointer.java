c2a7c890aafc1d0125f3c399c4122d39af2738fa
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2024-08-24 14:14:36 -0400

    Update CloneLinkedListWithRandomAndNextPointer.java

--- CloneLinkedListWithRandomAndNextPointer.java
+++ CloneLinkedListWithRandomAndNextPointer.java
@@ -47,4 +47,71 @@ class Solution {
         return map.get(head);
         
     }
+
+    /*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+    public void insertNode(Node head)
+    {
+         Node temp=head;
+        while(temp != null){
+            Node copy = new Node(temp.val);
+             copy.next=temp.next;
+         temp.next=copy;
+         temp=temp.next.next;
+        }
+    }
+
+    public void connectrandom(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            Node copy = temp.next;
+
+            if (temp.random != null) {
+                copy.random = temp.random.next;
+            }
+
+            temp = temp.next.next;
+        }
+    }
+
+    public Node getCopyNodeOpt(Node head) {
+        Node dummyNode = new Node(-1);
+        Node point = dummyNode;
+        Node temp = head;
+
+        while (temp != null) {
+            // create next node
+            point.next = temp.next;
+
+            // restore node
+            temp.next = temp.next.next;
+
+            // move fwd
+            temp = temp.next;
+            point = point.next;
+        }
+        return dummyNode.next;
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return null;
+        insertNode(head);
+        connectrandom(head);
+        return getCopyNode(head);
+    }
+
 }
