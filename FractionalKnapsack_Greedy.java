92da004dc40054c50ebe2080a1de7eb451128842
Sherry Ahuja <72737856+ahujasherry@users.noreply.github.com>
2025-01-28 10:25:29 -0500

    Update FractionalKnapsack_Greedy.java

--- FractionalKnapsack_Greedy.java
+++ FractionalKnapsack_Greedy.java
@@ -1,54 +1,49 @@
-//https://www.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1
 import java.util.*;
-class Item {
-  int value, weight;
-  Item(int x, int y) {
-    this.value = x;
-    this.weight = y;
-  }
-}
 
-class itemComparator implements Comparator<Item>
-{
-    @Override
-    public int compare(Item a, Item b) 
-    {
-        double r1 = (double)(a.value) / (double)(a.weight); 
-        double r2 = (double)(b.value) / (double)(b.weight); 
-        if(r1 < r2) return 1; 
-        else if(r1 > r2) return -1; 
-        else return 0; 
+class Item {
+    int value, weight;
+    Item(int value, int weight) {
+        this.value = value;
+        this.weight = weight;
     }
 }
-public class solve{
-    static double fractionalKnapsack(int W, Item arr[], int n) {
-        Arrays.sort(arr, new itemComparator()); 
-        
+
+public class Solve {
+
+    static double fractionalKnapsack(List<Integer> val, List<Integer> wt, int capacity) {
+        // Create a list of items using the value and weight lists
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < val.size(); i++) {
+            items.add(new Item(val.get(i), wt.get(i)));
+        }
+
+        // Sort items by value-to-weight ratio in descending order
+        items.sort((a, b) -> Double.compare((double) b.value / b.weight, (double) a.value / a.weight));
+
         int curWeight = 0; 
-        double finalvalue = 0.0; 
-        
-        for (int i = 0; i < n; i++) {
-       
-            if (curWeight + arr[i].weight <= W) {
-                curWeight += arr[i].weight;
-                finalvalue += arr[i].value;
-            }
-     
-            else {
-                int remain = W - curWeight;
-                finalvalue += ((double)arr[i].value / (double)arr[i].weight) * (double)remain;
+        double finalValue = 0.0;
+
+        // Process each item
+        for (Item item : items) {
+            if (curWeight + item.weight <= capacity) {
+                curWeight += item.weight;
+                finalValue += item.value;
+            } else {
+                int remain = capacity - curWeight;
+                finalValue += ((double) item.value / item.weight) * remain;
                 break;
             }
         }
-     
-        return finalvalue;
-        
+
+        return finalValue;
     }
-    public static void main(String args[])
-    {
-        int n = 3, weight = 50;
-        Item arr[] = {new Item (100,20),new Item(60,10),new Item(120,30)};
-        double ans = fractionalKnapsack(weight, arr, n);
-        System.out.println("The maximum value is "+ans);
+
+    public static void main(String[] args) {
+        List<Integer> val = Arrays.asList(100, 60, 120);
+        List<Integer> wt = Arrays.asList(20, 10, 30);
+        int capacity = 50;
+
+        double maxValue = fractionalKnapsack(val, wt, capacity);
+        System.out.println("The maximum value is " + maxValue);
     }
 }
